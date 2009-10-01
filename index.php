92262c3b0487bdb195ea18bf66053a3a3f2db6d7
wjames5 <will@tekimaki.com>
2009-10-01 14:17:02 +0000

    remove ref to non-existant copyright.txt file

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_meta/index.php,v 1.11 2009/10/01 13:45:44 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_meta/index.php,v 1.12 2009/10/01 14:17:02 wjames5 Exp $
 // Copyright (c) 2004 bitweaver Sample
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// All Rights Reserved. See below for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 // Initialization
